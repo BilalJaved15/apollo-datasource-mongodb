c897f8f159344104dd21a91e721a4bbe6933e2b0
Muhammad Bilal <mac@macs-MacBook-Pro.local>
2025-12-23 10:17:32 +0500

    apollo server 4 upgrade

--- src/datasource.js
+++ src/datasource.js
@@ -1,27 +1,21 @@
-import { DataSource } from 'apollo-datasource'
-import { ApolloError } from 'apollo-server-errors'
-import { InMemoryLRUCache } from 'apollo-server-caching'
+import { GraphQLError } from 'graphql'
+import { InMemoryLRUCache } from '@apollo/utils.keyvaluecache'
 
 import { createCachingMethods } from './cache'
 
-class MongoDataSource extends DataSource {
-  // https://github.com/apollographql/apollo-server/blob/master/packages/apollo-datasource/src/index.ts
-  constructor(collection) {
-    super()
-
+class MongoDataSource {
+  constructor(options) {
+    const { collection, config } = options
     const setUpCorrectly =
       typeof collection === 'object' && Object.keys(collection).length === 1
     if (!setUpCorrectly) {
-      throw new ApolloError(
+      throw new GraphQLError(
         'MongoDataSource constructor must be given an object with a single collection'
       )
     }
     this.collectionName = Object.keys(collection)[0] // eslint-disable-line
     this[this.collectionName] = collection[this.collectionName]
-  }
 
-  initialize(config) {
-    this.context = config.context
     const cache = config.cache || new InMemoryLRUCache()
 
     const { debug, allowFlushingCollectionCache } = config
